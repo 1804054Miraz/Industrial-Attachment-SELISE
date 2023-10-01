32659e89be84846d60d6536889d84e6f1a355595
1804054Miraz <u1804054@student.cuet.ac.bd>
2023-10-01 14:59:19 +0600

    add connectthree and location change of TS

---
+++ ConnectThree/src/app/level2/level2.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-level2',
+  templateUrl: './level2.component.html',
+  styleUrls: ['./level2.component.css']
+})
+export class Level2Component {
+  public testField = 'x';
+
+  public getTestField():string{
+    return 'o';
+  }
+}
