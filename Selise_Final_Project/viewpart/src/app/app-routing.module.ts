b6476c5a19aef48d8af782e50f3c0f989911c6c7
1804054Miraz <u1804054@student.cuet.ac.bd>
2023-10-27 08:06:36 +0600

    app-routing module modified

--- Selise_Final_Project/viewpart/src/app/app-routing.module.ts
+++ Selise_Final_Project/viewpart/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth.guard;
+import { AuthGuard } from './auth.guard';
 import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 import { SellerHomeComponent } from './seller-home/seller-home.component';
 import { SellerUpdateProductComponent } from './seller-update-product/seller-update-product.component';
+import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
+import { SearchComponent } from './search/search.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 
 const routes: Routes = [
   {
@@ -19,9 +26,14 @@ const routes: Routes = [
     component : SellerHomeComponent,
     path : 'seller-home',
     canActivate : [AuthGuard]
+  },
+  {
+    component:SellerAddProductComponent,
+    path:'seller-add-product',
+    canActivate:[AuthGuard]
   },{
-    component: SellerUpdateProductComponent
-    path:'seller-update-product/:id',
+    component: SellerUpdateProductComponent,
+    path:'',
     canActivate:[AuthGuard]
   },
   {
